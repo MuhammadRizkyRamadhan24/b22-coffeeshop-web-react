f67a8d588ff8bacb8311a939ac4889da05bb7954
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-06-24 18:46:36 +0700

    update helper

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,15 +1,11 @@
 import axios from "axios"
 
 export const http = (token=null) => {
-    const headers = {
-        // 'Content-Type': 'multipart/form-data',
-        // authorization: ''
-    }
+    const headers = {}
     if(token){
         headers.authorization = `Bearer ${token}`
-        console.log(headers)
     }
     return axios.create({
-        headers
+        headers 
     })
 }
